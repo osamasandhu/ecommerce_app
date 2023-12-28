7670c84764ac00a47111ff87a5863da45b40b788
Osama Sandhu <osamasandhu3636@gmail.com>
2023-12-28 09:12:02 +0500

    Named Routes

---
+++ lib/src/helpers/route_names.dart
@@ -0,0 +1,9 @@
+class AppRoute {
+  static String signIn= 'signIn';
+  static String signup= 'signUp';
+  static String category= 'category';
+  static String productList= 'productList';
+  static String productDetail= 'productDetail';
+  static String cart= 'cart';
+
+}
