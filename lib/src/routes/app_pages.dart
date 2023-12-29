9d4b5493b6a71f69f44ee31e3f6923c03d9c6ac0
Osama Sandhu <osamasandhu3636@gmail.com>
2023-12-29 14:49:52 +0500

    Advanced Enum

---
+++ lib/src/routes/app_pages.dart
@@ -0,0 +1,16 @@
+enum AppPages {
+  signIn(name: 'signIn', path: '/'),
+  signUp(name: 'signUp', path: 'signUp'),
+  category(name: 'categories', path: '/categories'),
+  products(name: 'products', path: '/products/category/:categoryName'),
+  productDetails(name: 'productDetail', path: '/product/:id'),
+  cart(name: 'cart', path: '/cart');
+
+  const AppPages({
+    required this.name,
+    required this.path,
+  });
+
+  final String name;
+  final String path;
+}
